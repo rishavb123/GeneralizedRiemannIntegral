813536fc5dbd8254b77eadf9430688f785e214ef
Rishav Bhagat <rishavb123@gmail.com>
2020-04-19 18:36:36 -0400

    testing partitions

--- test.py
+++ test.py
@@ -1,23 +1,44 @@
 from intervals import *
+from partitions import *
+from sys import argv
 
-A = Interval(0, 5)
-B = Interval(4, 10, closed=False)
+class Test:
 
-C = Interval.intersection(A, B)
-D = Interval.union(A, B)
+    @staticmethod
+    def intervals():
+        A = Interval(0, 5)
+        B = Interval(4, 10, closed=False)
 
-print(A, B, len(A), B.size())
-print(C, D)
-print(Union((C, D)))
-print()
+        C = Interval.intersection(A, B)
+        D = Interval.union(A, B)
 
-def test(I):
-    print(I)
-    print("Contains 4:", I.contains(4))
-    print("Contains 5:", 5 in I)
-    print()
+        print(A, B, len(A), B.size())
+        print(C, D)
+        print(Union((C, D)))
+        print()
 
-test(A)
-test(B)
-test(C)
-test(D)
+        def test(I):
+            print(I)
+            print("Contains 4:", I.contains(4))
+            print("Contains 5:", 5 in I)
+            print()
+
+        test(A)
+        test(B)
+        test(C)
+        test(D)
+    
+    @staticmethod
+    def partitions():
+        end = 10
+        I = Interval(0, end)
+        P = Partition([i for i in range(end + 1)], I)
+        T = P.tag([i + 0.5 for i in range(end)])
+
+        print("Interval:", I)
+        print("Partition:", P)
+        print("Tagged Partition:", T)
+
+for arg in argv[1:]:
+    print('\n---------------------------' + arg.upper() + ' TEST---------------------------')
+    getattr(Test, arg)()
