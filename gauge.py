96d289c4c5d3f9937df6cc84890ca3e950c2dc00
Rishav Bhagat <rishavb123@gmail.com>
2020-04-19 19:29:27 -0400

    created basic gauge class

---
+++ gauge.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from intervals import Interval
+
+class Gauge:
+
+    def __init__(self, func, interval):
+        self.delta = func
+        self.interval = interval
+        self.__validate__()
+
+    def __validate__(self):
+        for x in self.interval:
+            assert self.delta(x) > 0
+
+    def __call__(self, x):
+        assert self.interval.contains(x)
+        return self.delta(x)
+
+    @staticmethod
+    def constant(delta):
+        assert delta > 0
+        return Gauge(lambda:delta, Interval(-np.inf, np.inf, closed=False, iters=1))
