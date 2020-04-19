5c756634bd24ebae52a22f3e16f4e109fb993cef
Rishav Bhagat <rishavb123@gmail.com>
2020-04-19 19:30:04 -0400

    added discretizing functionality

--- intervals.py
+++ intervals.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class CustomInterval:
 
     def __contains__(self, item):
@@ -5,6 +7,13 @@ class CustomInterval:
 
     def __len__(self):
         return self.size()
+
+    def __iter__(self):
+        return self.discritize().__iter__()
+
+    def discretize(self):
+        print("Using default discritize method, please override this")
+        return []
 
     def contains(self, x):
         print("Using default contains method, please override this")
@@ -25,17 +34,39 @@ class CustomInterval:
 
 class Interval(CustomInterval):
 
-    def __init__(self, a, b, closed=True):
+    def __init__(self, a, b, closed=True, iters=1000):
         self.__a = a
         self.__b = b
         self.__closed = closed
         self.__size = b - a
+        self.__iters = iters
+        self.__discrete = None
+
+        self.__validate__()
+
+    def __validate__(self):
+        assert self.__b >= self.__a
 
     def contains(self, x):
         return self.__a <= x <= self.__b if self.__closed else self.__a < x < self.__b
 
     def size(self):
         return self.__size
+
+    def discretize(self):
+        if self.__discrete is None:
+            a = self.__a
+            b = self.__b
+            if a == b:
+                self.__discrete = []
+            else:
+                if abs(a) == np.inf:
+                    a = 9999999 * np.sign(a)
+                if abs(b) == np.inf:
+                    b = 9999999 * np.sign(b)
+                s = (b - a) / self.__iters
+                self.__discrete = list(np.arange(a, b + s, s) if self.__closed else np.arange(a + s, b, s))
+        return self.__discrete
 
     def __str__(self):
         return ('[' if self.__closed else '(') + str(self.__a) + ", " + str(self.__b) + (']' if self.__closed else ')')
@@ -44,19 +75,25 @@ class Interval(CustomInterval):
     def single_value(x):
         return Interval(x, x)
 
-    @staticmethod
-    def empty():
-        return Interval(0, 0)
-
 class Union(CustomInterval):
 
     def __init__(self, intervals):
         self.intervals = intervals
+        self.__discrete = None
 
     def contains(self, x):
         for i in self.intervals:
             if i.contains(x): return True
         return False
+
+    def discretize(self):
+        if self.__discrete is None:
+            self.__discrete = []
+            for interval in self.intervals:
+                self.__discrete += interval.discretize()
+            self.__discrete = list(set(self.__discrete))
+            self.__discrete.sort()
+        return self.__discrete
 
     def __str__(self):
         return '(' + ') U ('.join([str(i) for i in self.intervals]) + ')'
@@ -65,11 +102,20 @@ class Intersection(CustomInterval):
 
     def __init__(self, intervals):
         self.intervals = intervals
+        self.__discrete = None
 
     def contains(self, x):
         for i in self.intervals:
             if not i.contains(x): return False
         return True
+
+    def discretize(self):
+        if self.__discrete is None:
+            self.__discrete = []
+            for val in Union(self.intervals).discretize():
+                if self.contains(val):
+                    self.__discrete.append(val)
+        return self.__discrete
 
     def __str__(self):
         return '(' + ') n ('.join([str(i) for i in self.intervals]) + ')'
@@ -84,3 +130,6 @@ def C(CustomInterval):
 
     def __str__(self):
         return '(' + str(self.interval) + ')^C'
+
+EMPTY = Interval(0, 0)
+R = Interval(-np.inf, np.inf)
